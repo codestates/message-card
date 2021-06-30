536aa20cafe8ae9bf4456ee9d47220114b18da5f
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-06-30 21:19:11 +0900

    펀치홀 수정

--- index.js
+++ index.js
@@ -14,46 +14,52 @@ let result_3 = document.querySelector('#result_3');
 let result_4 = document.querySelector('#result_4');
 
 start_btn.onclick = function startHandler () {
-    // ! (4단계) 시작페이지에서 start 버튼을 클릭했을 때 실행되는 코드입니다.
-    
+    start_page.style.display = 'none';
+    step_1.style.display = 'block';
 }
 
 step_1_A.onclick = function step1_A_Answer () {
     result.push('1-A')
-    // ! (4단계) Step_1 페이지에서 A 선택지를 클릭했을 때 실행되는 코드입니다.
 
+    step_1.style.display = 'none';
+    step_2.style.display = 'block';
 }
 
 step_1_B.onclick = function step1_B_Answer () {
     result.push('1-B')
-    // ! (4단계) Step_1 페이지에서 B 선택지를 클릭했을 때 실행되는 코드입니다.
 
+    step_1.style.display = 'none';
+    step_2.style.display = 'block';
 }
 
+//TODO: 여기서부터 자바스크립트로 조건문을 작성해야 합니다.
+
+// * step 2에서 A를 클릭한 경우 아래 코드가 실행됩니다.
 step_2_A.onclick = function step2_A_Answer () {
     result.push('2-A')
 
     step_2.style.display = 'none';
-
+   
     if(result[0] === '1-A') {
-        result_1.style.display = 'block';
-        // console.log(result)
+        //! (1단계) 결과 값이 1-A, 2-A 일 때, result_1 화면을 보여주세요.
+        결과_화면.style.display = 'block';
     } else if(result[0] === '1-B') {
-        result_3.style.display = 'block'
-        // console.log(result)
+        //! (2단계) 결과 값이 1-B, 2-A 일 때, result_2 화면을 보여주세요.
+        결과_화면.style.display = 'block';
     }
 }
 
+// * step 2에서 B를 클릭한 경우 아래 코드가 실행됩니다.
 step_2_B.onclick = function step2_B_Answer () {
     result.push('2-B')
 
     step_2.style.display = 'none';
 
     if(result[0] === '1-A') {
-        result_2.style.display = 'block';
-        // console.log(result)
+        //! (3단계) 결과 값이 1-A, 2-B 일 때, result_3 화면을 보여주세요.
+        결과_화면.style.display = 'block';
     } else if(result[0] === '1-B') {
-        result_4.style.display = 'block'
-        // console.log(result)
+        //! (4단계) 결과 값이 1-B, 2-B 일 때, result_4 화면을 보여주세요.
+        결과_화면.style.display = 'block';
     }
 }
